6b2d391d639558add7b6ac6c7c6984c19a586e45
Seth J Landry <waymakerijn@outlook.com>
2024-12-02 16:16:03 -0600

    Initialize delete-basefee

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const Paytr = artifacts.require("Paytr.sol");
+
+module.exports = function (deployer) {
+  deployer.deploy(Paytr, "0x83C766237dD04EB47F62784218839F892A691E84","0xc3d688B66703497DAA19211EEdff47f25384cdc3",6,"0x131eb294E3803F23dc2882AB795631A12D1d8929","0xFC078A6812eb56320f879A153A0bd23C34D9B508");
+};
