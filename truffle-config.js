fcc1349f0992848882c416351390b211e4741bf6
Seth J Landry <waymakerijn@outlook.com>
2024-12-02 16:16:03 -0600

    Initialize Chainlink-pricefeed

---
+++ truffle-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  compilers: {
+    solc: {
+      version: "^0.8.19", 
+    }
+  },
+  networks: {
+    myFork: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "5", // match any network
+      websockets: true,
+    },  
+  },
+  dashboard: {
+    port: 24012,  
+  }
+}
